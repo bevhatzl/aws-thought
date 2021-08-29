3439798558c8d3062702cdb5a0b416348bc5edb1
bevhatzl <beverleyhatzl@gmail.com>
2021-08-29 15:06:51 +1000

    integrate api

---
+++ server/utils/params-config.js
@@ -0,0 +1,17 @@
+// This file has a singular purpose: to return a configured params object. We'll use a package called uuid that will generate a unique 36-character alphanumeric string, which we'll use as the image file names.
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: 'user-images-7f6a2a9f-699a-40b8-b557-a28634f43d7b',
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
